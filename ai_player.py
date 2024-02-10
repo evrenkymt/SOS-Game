34e01cdac73e9f04bcde20b80629ef1b7e859771
Evren Koymat <koyymat@gmail.com>
2024-02-10 21:49:21 +0300

    ai_player & sos_board updated

--- ai_player.py
+++ ai_player.py
@@ -7,20 +7,50 @@ class AI_Players(Players):
     def get_move(self):
         raise NotImplementedError('AI players dont take input from user. They make their own move.')
     
-    def minimax(self, board, depth, is_maximizing):
-        # check there are no more empty spaces or depth limit is reached.
+    def minimax(self, board, depth, is_maximizing, player_one_score, player_two_score):
+        # Check if the game is over or the depth limit is reached
         if board.is_full() or depth == 0:
-            return self.evaluate(board), None
-        
+            return board.evaluate(player_one_score, player_two_score), None   # evaluate function will be added !!
+
         if is_maximizing:
             max_eval = float('-inf')
             best_move = None
-            
+
             for move in board.get_available_moves():
-                # should be a copy of the board
-                new_board = board
-                new_board.update_board(move)
+                board.update_board(move)
                 
-                eval, _ = self.minimax(new_board, depth - 1, False)
+                player_one_score, sos = board.check_minimax_sos(move, player_one_score)
                 
+                eval, _ = self.minimax(board, depth - 1, False, player_one_score, player_two_score)
+                board.undo_move(move)
+                player_one_score -= sos
+
+                if eval > max_eval:
+                    max_eval = eval
+                    best_move = move
+
+            return max_eval, best_move
+        else:
+            min_eval = float('inf')
+            best_move = None
+
+            for move in board.get_available_moves():
+                board.update_board(move)
+                
+                player_two_score, sos = board.check_minimax_sos(move, player_two_score)
+                
+                eval, _ = self.minimax(board, depth - 1, True, player_one_score, player_two_score)
+                board.undo_move(move)
+                player_two_score -= sos
+                
+
+                if eval < min_eval:
+                    min_eval = eval
+                    best_move = move
+
+            return min_eval, best_move
+
+    def get_best_move(self, board, depth=4):
+        _, best_move = self.minimax(board, depth, True, 0, 0)
+        return best_move
                 
--- sos_board.py
+++ sos_board.py
@@ -14,6 +14,10 @@ class SOS_BOARD:
     
     def update_board(self, move):
         self.board[move[0]][move[1]] = move[2]
+        return self.board
+    
+    def evaluate(self, player_one_score, player_two_score):
+        return player_one_score - player_two_score
 
     def check_move(self, move):
         if move[0] > self.row or move[0] < 0 or move[1] > self.col or move[1] < 0:
@@ -40,6 +44,10 @@ class SOS_BOARD:
                     available_moves.append([i, j, 'S'])
                     available_moves.append([i, j, 'O'])
         return available_moves
+    
+    def undo_move(self, move):
+        self.board[move[0]][move[1]] = ' '
+        return self.board
     
     def check_sos(self, move, player):
         move_row = move[0]
@@ -87,6 +95,45 @@ class SOS_BOARD:
             return True
         else:
             return False
+    
+    def check_minimax_sos(self, move, player_score):
+        move_row = move[0]
+        move_col = move[1]
+        move_letter = move[2]
         
+        sos_count = 0
         
-        
+        if move_letter == 'O':
+            # check row : 'SOS'
+            if (move_col >= 1 and move_col <= self.col - 2) and self.board[move_row][move_col - 1] == 'S' and self.board[move_row][move_col + 1] == 'S':
+                sos_count += 1
+            # check column: 'SOS'
+            if (move_row >= 1 and move_row <= self.row - 2) and self.board[move_row - 1][move_col] == 'S' and self.board[move_row + 1][move_col] == 'S':
+                sos_count += 1
+            # check diagonally top left to bottom right: 'SOS'
+            if (move_row >= 1 and move_col >= 1 and move_row <= self.row - 2 and move_col <= self.col - 2) and self.board[move_row - 1][move_col - 1] == 'S' and self.board[move_row + 1][move_col + 1] == 'S':
+                sos_count += 1
+            # check diagonally top right to bottom left: 'SOS'
+            if (move_row >= 1 and move_row <= self.row - 2 and move_col >= 1 and move_col <= self.col - 2) and self.board[move_row - 1][move_col + 1] == 'S' and self.board[move_row + 1][move_col - 1] == 'S':
+                sos_count += 1
+        elif move_letter == 'S':
+            # check 2 left: 'SOS'
+            if move_col >= 2 and self.board[move_row][move_col - 1] == 'O' and self.board[move_row][move_col - 2] == 'S':
+                sos_count += 1
+            # check 2 right: 'SOS'
+            if move_col <= self.col -3 and self.board[move_row][move_col + 1] == 'O' and self.board[move_row][move_col + 2] == 'S':
+                sos_count += 1
+            # check 2 up: 'SOS'
+            if move_row >= 2 and self.board[move_row - 1][move_col] == 'O' and self.board[move_row - 2][move_col] == 'S':
+                sos_count += 1
+            # check 2 down: 'SOS'
+            if move_row <= self.row - 3 and self.board[move_row + 1][move_col] == 'O' and self.board[move_row + 2][move_col] == 'S':
+                sos_count += 1
+            # check diagonally bottom right S to top left: 'SOS'
+            if move_row >= 2 and move_col >= 2 and self.board[move_row - 1][move_col - 1] == 'O' and self.board[move_row - 2][move_col - 2] == 'S':
+                sos_count += 1
+            # check diagonally bottom left S to top right: 'SOS'
+            if move_row >= 2 and move_col <= self.col - 3 and self.board[move_row - 1][move_col + 1] == 'O' and self.board[move_row - 2][move_col + 2] == 'S':
+                sos_count += 1
+        player_score += sos_count
+        return player_score, sos_count
